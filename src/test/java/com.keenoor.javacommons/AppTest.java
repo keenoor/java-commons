74296ff654f76988817745cc3433c67e68ea8dde
chenliuchun <chenliuchun@zccp.com>
2018-08-29 15:03:16 +0800

    update gradle script

--- src/test/java/com.keenoor.javacommons/AppTest.java
+++ src/test/java/com.keenoor.javacommons/AppTest.java
@@ -2,19 +2,40 @@ package com.keenoor.javacommons;
 
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+
 import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
     }
+
+    @Test
+    public void testJDKEncode() {
+        System.out.println("default Charset=" + Charset.defaultCharset());
+        System.out.println("file.encoding=" + System.getProperty("file.encoding"));
+        System.out.println("sun.jnu.encoding=" + System.getProperty("sun.jnu.encoding"));
+        System.out.println("default Charset OutputStreamWriter=" + new OutputStreamWriter(new ByteArrayOutputStream()).getEncoding());
+
+    }
+
+    @Test
+    public void testArray(){
+
+        String[] strings = {"aa", "bb", "cc"};
+        String s = Arrays.toString(strings);
+        System.out.println(s);
+    }
+
 }
