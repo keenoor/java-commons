65fe1692a1a1a2ab75eb35de0f3296567a4c6f9f
chenliuchun <chenliuchun@zccp.com>
2018-08-29 15:02:43 +0800

    update regex

---
+++ src/test/java/com.keenoor.javacommons/utils/RegexUtilsTest.java
@@ -0,0 +1,29 @@
+package com.keenoor.javacommons.utils;
+
+import com.keenoor.javacommons.utils.constant.RegexConstants;
+
+import org.junit.Test;
+
+/**
+ * @author: chenliuchun
+ * Date:        2018/8/17
+ * Description:
+ * Modification History:
+ * Date       Author       Version     Description
+ * -----------------------------------------------------
+ */
+public class RegexUtilsTest {
+
+    private final String regex = "^http://" + RegexConstants.REGEX_IP + ":" + RegexConstants.REGEX_PORT + "(/.*)?";
+
+    @Test
+    public void isMatch() {
+        String url = "http://192.168.2.88:8080/asda-saf";
+        boolean match = RegexUtils.isMatch(regex, url);
+        System.out.println(match);
+    }
+
+    @Test
+    public void getMatches() {
+    }
+}
